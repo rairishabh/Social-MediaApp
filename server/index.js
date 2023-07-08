d0c6c80bc3c50ee567e9a06b928f3ebe1947659f
Rishabh Rai <75465666+rairishabh@users.noreply.github.com>
2023-07-08 14:14:31 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,12 +11,7 @@ const cors = require("cors");
 const cloudinary = require('cloudinary').v2;
 dotenv.config("./.env");
 
-//configuration
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-});
+
 
 // middlewares
 app.use(express.json({ limit: '50mb' }));
